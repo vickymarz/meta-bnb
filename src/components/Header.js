3a291c3bfb1d82a2edf084e0d51b06e4ad8e421d
vickymarz <okorojiebube2@gmail.com>
2022-11-11 13:09:07 +0100

    Add header component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,45 @@
-import React from 'react';
+import React, { useState } from 'react';
+import MediaQuery from 'react-responsive/types/Component';
 import logo from '../images/logo.png'
 
 const Header = () => {
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
+    const handleMenuOpen = () => {
+		setIsMenuOpen(!isMenuOpen);
+	};
   return (
     <header>
-        <div>
-            <img src={logo} alt="logo" />
-        </div>
+      <MediaQuery maxWidth={768}>
+        {matches =>
+          matches ? (
+            <>
+              <div>
+                <img src={logo} alt="logo" />
+              </div>
+              {isMenuOpen ? (
+			    <Button onClick={handleMenuOpen} isMenuOpen>
+				  <FontAwesomeIcons icon={faTimes} size='1x' />
+				</Button>
+				) : (
+				<Button onClick={handleMenuOpen}>
+				  <FontAwesomeIcons icon={faAlignRight} size='1x' />
+				</Button>
+				)
+              }
+              <MobileMenu />
+            </>
+          ) :
+          (
+            <>
+              <Navbar />
+            </>
+          )
+        }
+        </MediaQuery>
     </header>
+
   )
 }
 
 export default Header
+
