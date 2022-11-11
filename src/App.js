641652a33f2d20762a0a78c7cf8d33f530ceb674
vickymarz <okorojiebube2@gmail.com>
2022-11-11 13:50:45 +0100

    Handle route error

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React from "react";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import ErrorPage from "./components/ErrorPage";
 import Header from "./components/Header";
+import Homepage from "./pages/Homepage";
+import Locations from "./pages/Locations";
 
 const router = createBrowserRouter([
   {
