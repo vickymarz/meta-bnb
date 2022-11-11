95c75410ceca805cdee1019814afce090ecf4085
vickymarz <okorojiebube2@gmail.com>
2022-11-11 12:18:16 +0100

    Add global styles

---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from '../images/logo.png'
+
+const Header = () => {
+  return (
+    <header>
+        <div>
+            <img src={logo} alt="logo" />
+        </div>
+    </header>
+  )
+}
+
+export default Header
